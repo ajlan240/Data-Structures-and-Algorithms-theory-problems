0500a11c32498153fc55512a750189d12c26011a
Ajlan Ashraf <ajlanashrafv@gmail.com>
2025-07-02 13:40:54 +0530

    BST Insertion algorithm

--- src/DSA_Concepts/Trees/BtMain.java
+++ src/DSA_Concepts/Trees/BtMain.java
@@ -4,9 +4,15 @@ import java.util.Scanner;
 
 public class BtMain {
     public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree();
-        Scanner scanner = new Scanner(System.in);
-        tree.populate(scanner);
-        tree.display();
+//        BinaryTree tree = new BinaryTree();
+//        Scanner scanner = new Scanner(System.in);
+//        tree.populate(scanner);
+//        tree.display();
+
+        int[] arr = {5, 2, 7, 1, 4, 6, 9, 8, 3, 10};
+        BinarySearchTree bsTree = new BinarySearchTree();
+        bsTree.populate(arr);
+        bsTree.display();
+
     }
 }
---
+++ src/DSA_Concepts/Trees/TraversalAlgorithm.java
@@ -0,0 +1,8 @@
+package DSA_Concepts.Trees;
+
+public class TraversalAlgorithm {
+    // 1. pre-order : N -> L -> R
+    // 2. in-order : L -> N -> R
+    // 3. Post-order : L -> R -> N
+
+}
